3c0b74d83076f4fcddd120024f3fae666198db70
Reid Blomquist <reid@theprogrampdx.com>
2016-06-22 12:57:55 -0700

    tweak can pos

--- src/components/cans.js
+++ src/components/cans.js
@@ -8,10 +8,10 @@ export default props => (
     <Entity obj-model={{obj: '#can-obj', mtl:'#can-mtl'}}
            scale="0.15 0.15 0.15" position="-0.5 -1.5 -5.5"/>
     <Entity obj-model={{obj: '#can-obj', mtl:'#can-mtl'}}
-           scale="0.15 0.15 0.15" position="1 -1.5 -4"
+           scale="0.15 0.15 0.15" position="1 -1.7 -4"
            rotation="90 0 30"/>
     <Entity obj-model={{obj: '#can-obj', mtl:'#can-mtl'}}
-           scale="0.15 0.15 0.15" position="-2 -1.5 -4.6"
+           scale="0.15 0.15 0.15" position="-2 -1.7 -4.6"
            rotation ="90 0 123"/>
   </Entity>
 );
